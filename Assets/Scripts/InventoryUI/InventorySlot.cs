f82075bddabcbb7c25f3bfb02a38ade190458c07
Crabzillah <enity94@gmail.com>
2023-10-26 18:08:57 +0300

    Minor tweaks for InventoryUI
    + Statue placeholder
    @DevScene

--- Assets/Scripts/InventoryUI/InventorySlot.cs
+++ Assets/Scripts/InventoryUI/InventorySlot.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class InventorySlot : MonoBehaviour
 {
     public Image icon;
     public Button removeButton;
+    public GameObject rubyText;
+    public GameObject fileText;
+    public GameObject pillText;
 
     Item item; //this variable will keep track of the current item in the slot
 
+    //private bool showRubyText = false;
 
     public void AddItem(Item newItem)
     {
@@ -37,7 +42,35 @@ public class InventorySlot : MonoBehaviour
     {
         if(item != null)
         {
-            item.Use();
+            if (item.name == "Red Ruby")
+            {
+                item.Use();
+
+                rubyText.SetActive(!rubyText.activeInHierarchy); // activate/deactivate ruby description text panel
+                fileText.SetActive(false);
+                pillText.SetActive(false);
+
+
+            }
+            if (item.name == "File")
+            {
+                item.Use();
+
+                fileText.SetActive(!fileText.activeInHierarchy); // activate/deactivate ruby description text panel
+                pillText.SetActive(false);
+                rubyText.SetActive(false);
+
+            }
+            if (item.name == "Pill")
+            {
+                item.Use();
+
+                pillText.SetActive(!pillText.activeInHierarchy); // activate/deactivate ruby description text panel
+                rubyText.SetActive(false);
+                fileText.SetActive(false);
+
+            }
+
         }
     }
 
